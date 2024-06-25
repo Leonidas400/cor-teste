f459f6ddfe15aed425b1b99ae8469857898e2796
Leonidas400 <dilvano639@gmail.com>
2024-06-25 20:38:50 -0300

    adicionando opções

--- caixa.js
+++ caixa.js
@@ -1,6 +1,19 @@
 let box = document.querySelector("div"), input = document.querySelector("input");
+let $cor= document.querySelectorAll(".mudarCor")
+
+$cor.forEach(link => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault();
+        let newColor = link.textContent; // Pegue o texto dentro da tag a
+        box.style.backgroundColor = newColor;
+    });
+});
+
+
 
 input.addEventListener("input", () => {
     box.style.borderRadius = input.value;
     box.style.background = input.value;
 })
+
+
