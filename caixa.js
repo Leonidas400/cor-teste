f66a05924a4c7e66bc8e7ff15627f93ae1e06420
Leonidas400 <dilvano639@gmail.com>
2024-09-07 12:08:31 -0300

    melhorando o js

--- caixa.js
+++ caixa.js
@@ -1,11 +1,29 @@
-let box = document.querySelector(".caixa-cosmog"), input = document.querySelector(".cosmog");
+let box = document.querySelector(".caixa-cosmog");
+let inputCor = document.querySelector(".cosmogCor");
+let inputPalheta = document.querySelector(".cosmogPalheta");
+let inputBorda = document.querySelector(".cosmogBorda");
 let $estilo= document.querySelectorAll(".estilo")
 
-input.addEventListener("input", () => {
-    
-    box.style.borderRadius = input.value;
-    box.style.background = input.value;
-})
+function inputaCorEBorda(Cor, Borda, palheta){
+
+    let mudaEstilo = box.style;
+
+    palheta.addEventListener("input", ()=>{
+        mudaEstilo.background = palheta.value
+    })
+
+    Cor.addEventListener("input", () => {
+        mudaEstilo.background = Cor.value;
+    });
+
+    Borda.addEventListener("input", () => {
+        mudaEstilo.borderRadius = Borda.value;
+    });  
+}
+
+inputaCorEBorda(inputCor, inputBorda, inputPalheta);
+
+
 
 function mudaCorEBorda(estilizar){
     estilizar.forEach(link => {
